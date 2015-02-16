fc18661702ea01a5eadf065fbaeabaa0d8482862
Arnav Gosain <gosain.arnav@gmail.com>
2015-02-16 11:29:42 -0500

    Revert "blu_plug: fixes and cleanup"
    This reverts commit b27a049d499d16892d5d17f0a741febf7cb714ed.

--- arch/arm/mach-msm/blu_plug.c
+++ arch/arm/mach-msm/blu_plug.c
@@ -41,6 +41,7 @@
 #define MAX_FREQ_PLUG (2649600)
 #define MAX_CORES_PLUG (4)
 
+
 static unsigned int up_threshold = UP_THRESHOLD;;
 static unsigned int delay = DELAY;
 static unsigned int min_online = MIN_ONLINE;
@@ -53,6 +54,7 @@ static unsigned int max_cores_screenoff = MAX_CORES_SCREENOFF;
 static unsigned int max_freq_screenoff = MAX_FREQ_SCREENOFF;
 static unsigned int max_freq_plug = MAX_FREQ_PLUG;
 static unsigned int max_cores_plug = MAX_CORES_PLUG;
+bool prevsaver = false;
 static unsigned int rcrc;
 
 static struct delayed_work dyn_work;
@@ -61,14 +63,21 @@ static struct work_struct suspend, resume, touchy;
 static struct notifier_block notify;
 
 
-/* Bring online each possible CPU up to max_online cores */
-static inline void up_all(void)
+/*
+ * Bring online each possible CPU up to max_online threshold if lim is true or
+ * up to num_possible_cpus if lim is false
+ */
+static inline void up_all(bool lim)
 {
 	unsigned int cpu;
 
+	unsigned int max = (lim ? max_online : num_possible_cpus());
+
 	for_each_possible_cpu(cpu)
-		if (cpu_is_offline(cpu) && num_online_cpus() < max_online)
+		if (cpu_is_offline(cpu) && num_online_cpus() < max)
 			cpu_up(cpu);
+
+	down_timer = 0;
 }
 
 /* Put offline each possible CPU down to min_online threshold */
@@ -177,11 +186,10 @@ static __ref void load_timer(struct work_struct *work)
  */
 static __ref void max_screenoff(bool screenoff)
 {
-	uint32_t cpu, freq;
+	uint32_t cpu;
 	
 	if (screenoff) {
 		max_freq_plug = cpufreq_quick_get_max(0);
-		freq = max_freq_screenoff;
 			
 		max_cores_plug = max_online;
 		max_online = max_cores_screenoff;
@@ -195,11 +203,9 @@ static __ref void max_screenoff(bool screenoff)
 		cpufreq_update_policy(cpu);
 	}
 	else {
-		freq = max_freq_plug;
-		
 		max_online = max_cores_plug;
 		
-		up_all();
+		up_all(true);
 		
 		for_each_possible_cpu(cpu) {
 			msm_cpufreq_set_freq_limits(cpu, MSM_CPUFREQ_NO_LIMIT, max_freq_plug);
@@ -367,7 +373,7 @@ static __ref int set_min_online(const char *val, const struct kernel_param *kp)
 	ret = param_set_uint(val, kp);
 	
 	if (ret == 0) {
-			up_all();
+			up_all(true);
 	}
 	
 	return ret;
@@ -396,7 +402,7 @@ static __ref int set_max_online(const char *val, const struct kernel_param *kp)
 	
 	if (ret == 0) {
 		down_all();
-		up_all();
+		up_all(true);
 	}
 	
 	return ret;
@@ -418,7 +424,7 @@ static __ref int set_max_cores_screenoff(const char *val, const struct kernel_pa
 	ret = kstrtouint(val, 10, &i);
 	if (ret)
 		return -EINVAL;
-	if (i < 1 || i < min_online || i > max_online || i > num_possible_cpus())
+	if (i < 1 || i > num_possible_cpus())
 		return -EINVAL;
 	if (i > max_online)
 		max_cores_screenoff = max_online;
@@ -427,7 +433,7 @@ static __ref int set_max_cores_screenoff(const char *val, const struct kernel_pa
 	
 	if (ret == 0) {
 		down_all();
-		up_all();
+		up_all(true);
 	}
 	
 	return ret;
@@ -449,7 +455,7 @@ static __ref int set_max_freq_screenoff(const char *val, const struct kernel_par
 	ret = kstrtouint(val, 10, &i);
 	if (ret)
 		return -EINVAL;
-	if (i < 300000 || i > 2649600)
+	if (i < 300000 || i > 2265600)
 		return -EINVAL;
 
 	ret = param_set_uint(val, kp);
