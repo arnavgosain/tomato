4834448392d2a9de0ae0b8b4cbb9bab2ee9c9c08
Arnav Gosain <gosain.arnav@gmail.com>
2015-02-16 09:19:46 -0500

    Revert "cpufreq: impulse: added the new governor from @neobuddy89 kernel"
    This reverts commit eed96d33ab0dff2f3740e818627da3925d0ddaaf.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -437,9 +437,6 @@ extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
 extern struct cpufreq_governor cpufreq_gov_wheatley;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_wheatley)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
-extern struct cpufreq_governor cpufreq_gov_impulse;
-#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_impulse)
 #endif
 
 /*********************************************************************
